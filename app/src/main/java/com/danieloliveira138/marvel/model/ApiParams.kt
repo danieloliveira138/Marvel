a17557fc591c04194ce1c14d7fa57a039435c07c
Daniel Oliveira <daniel.j.oliveira.3@globalhitss.com.br>
2020-04-18 23:21:41 -0300

    Md5 and api base params.

---
+++ app/src/main/java/com/danieloliveira138/marvel/model/ApiParams.kt
@@ -0,0 +1,7 @@
+package com.danieloliveira138.marvel.model
+
+class ApiParams(
+    val ts: String,
+    val key: String,
+    val md5Hash: String
+)
---
+++ app/src/main/java/com/danieloliveira138/marvel/service/MarvelApiParams.kt
@@ -0,0 +1,17 @@
+package com.danieloliveira138.marvel.service
+
+import cc.duduhuo.util.digest.Digest
+import com.danieloliveira138.marvel.model.ApiParams
+import java.util.Calendar
+
+fun createApiParams(): ApiParams {
+    val timeInMillis = Calendar.getInstance().timeInMillis.toString()
+    val privateKey = "PRIVATE_KEY"
+    val publicKey = "PUBLIC_KEY"
+
+    return ApiParams(
+        ts = timeInMillis,
+        key = publicKey,
+        md5Hash = Digest.md5Hex("$timeInMillis$privateKey$publicKey")
+    )
+}
