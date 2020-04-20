bec7f8c0c40c69674e5339477c5c5da8ba2f6c07
Daniel Oliveira <daniel.j.oliveira.3@globalhitss.com.br>
2020-04-20 06:06:39 -0300

    Add Timber Libraries.

--- app/src/main/java/com/danieloliveira138/marvel/MainApplication.kt
+++ app/src/main/java/com/danieloliveira138/marvel/MainApplication.kt
@@ -7,12 +7,17 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import timber.log.Timber
 
 @Suppress("unused")
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         startKoin {
             androidLogger(level = if (BuildConfig.DEBUG) Level.DEBUG else Level.ERROR)
--- app/src/main/java/com/danieloliveira138/marvel/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/danieloliveira138/marvel/presentation/main/MainViewModel.kt
@@ -1,11 +1,11 @@
 package com.danieloliveira138.marvel.presentation.main
 
 import android.content.Context
-import android.util.Log
 import com.danieloliveira138.marvel.model.Status
 import com.danieloliveira138.marvel.presentation.BaseViewModel
 import com.danieloliveira138.marvel.service.Repository
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class MainViewModel(
     private val context: Context,
@@ -17,10 +17,10 @@ class MainViewModel(
 
             when (response.status) {
                 Status.SUCCESS -> {
-                    Log.d("CATRA","${response.data}")
+                    Timber.d("CATRA\n${response.data}")
                 }
                 Status.ERROR -> {
-                    Log.d("CATRA ERROR", "\n${response.message}\n${response.exception}")
+                    Timber.d("CATRA ERROR\n${response.message}\n${response.exception}")
                 }
             }
         }
